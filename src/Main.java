2e3a545e949293a7963bf9f6321d749063c66463
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-09 12:20:06 -0300

    Learning Java ./Main.java - 2023-03-09 12:20:06

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    /*
+    * Arithmetic operators:
+    * + - Plus - e.g. 1 + 1 = 2
+    * - - Minus - e.g. 2 - 1 = 1
+    * * - Multiplication - e.g. 3 * 2 = 6
+    * / - Division - e.g. 6 / 2 = 3
+    * % - Modulus / Rest of division - e.g. 6 % 2 = 0
+    * */
+
+    /*
+    * Variables declaration - Syntax:
+    * <type> <name> = <initial value>
+    * int age = 20;
+    * String name = "lucas";
+    * */
+
+    /*
+    * Primitive types - Java:
+    * Integer numeric types:
+    * type | size | values | default value
+    * byte | 8 bits | -128 - 127 | 0
+    * short | 16 bits | -32768 - 32767 | 0
+    * int | 32 bits | -2147483648 - 2147483647 | 0
+    * long | 64 bits | -9223372036854775808 - 9223372036854775807 | 0L
+    *
+    * Floating point numeric types:
+    * type | size | values | default value
+    * float | 32 bits | -1.40239846x10^-45 - 3.40282347x10^38 | 0.0f
+    * double | 64 bits | 4.9406564584124654x10^-324 - 1.7976931348623157x10^308 | 0.0d
+    *
+    * Unicode character type:
+    * type | size | values | default value
+    * char | 16 bits | '\u0000' - '\uFFFF' | '\u0000' | - https://unicode-table.com/pt/
+    *
+    * Boolean type:
+    * type | size | values | default value
+    * boolean | 1 bit | true or false | false
+    * */
+
+    /*
+    * Non-primitive types - Java:
+    * String - https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/String.html
+    * String is a sequence of characters - e.g. "Hello world!" - https://unicode-table.com/pt/
+    * */
+}
