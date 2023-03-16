16a43143bfc24db54507a5f78c726daf936c3d20
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-16 10:07:06 -0300

    Learning Java ./conditionalStructure/logicExpressions/logicExpressions.java - 2023-03-16 10:07:06

---
+++ src/conditionalStructure/logicExpressions/logicExpressions.java
@@ -0,0 +1,11 @@
+package conditionalStructure.logicExpressions;
+
+public class logicExpressions {
+    public static void main(String[] args) {
+        /*
+        * && = AND
+        * || = OR
+        * ! = NOT => int x = 10 => !(x == 10) => false
+        * */
+    }
+}
