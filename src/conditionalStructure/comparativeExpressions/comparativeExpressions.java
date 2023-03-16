6d4e2f93ed4810122f66f03512484d6bf2318a39
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-16 10:00:16 -0300

    Learning Java ./conditionalStructure/comparativeExpressions/comparativeExpressions.java - 2023-03-16 10:00:16

---
+++ src/conditionalStructure/comparativeExpressions/comparativeExpressions.java
@@ -0,0 +1,14 @@
+package conditionalStructure.comparativeExpressions;
+
+public class comparativeExpressions {
+    public static void main(String[] args) {
+        /*
+        * > - Greater than
+        * < - Less than
+        * <= - Less or equal than
+        * >= - Greater or equal than
+        * == - Equal to
+        * != - Not equal to
+        * */
+    }
+}
