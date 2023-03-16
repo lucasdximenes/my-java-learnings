8b6ff5af2915028fe4153200d89cf8e33f0f466e
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-16 12:31:45 -0300

    Learning Java ./conditionalStructure/ternaryOperator/ternaryOperator.java - 2023-03-16 12:31:45

---
+++ src/conditionalStructure/ternaryOperator/ternaryOperator.java
@@ -0,0 +1,28 @@
+package conditionalStructure.ternaryOperator;
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+        // Syntax:
+        // (condition) ? value_if_true : value_if_false;
+
+        //examples:
+        //* (2 > 4) ? 50 : 80;
+        //* (10 != 3) ? "Maria" : "Alex";
+
+        /*
+        * double price = 34.5;
+        * double discount;
+        * if (price < 20.00) {
+        *     discount = price * 0.1;
+        * } else {
+        *     discount = price * 0.05;
+        * }
+        */
+
+        /*
+        * double price = 34.5;
+        * double discount = (price < 20.0) ? price * 0.1 : price * 0.05;
+        * */
+
+    }
+}
