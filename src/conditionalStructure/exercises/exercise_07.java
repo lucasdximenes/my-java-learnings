4a5f9886fc5d91ff06c774183145be59376e5116
Lucas Ximenes <lucasdximenes@gmail.com>
2023-03-16 11:12:01 -0300

    Learning Java ./conditionalStructure/exercises/exercise_07.java - 2023-03-16 11:12:01

---
+++ src/conditionalStructure/exercises/exercise_07.java
@@ -0,0 +1,30 @@
+package conditionalStructure.exercises;
+
+import java.util.Scanner;
+
+public class exercise_07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float x = sc.nextFloat();
+        float y = sc.nextFloat();
+
+        if (x > 0.0 && y > 0.0) {
+            System.out.println("Q1");
+        } else if (x > 0.0 && y < 0.0) {
+            System.out.println("Q4");
+        } else if (x < 0.0 && y > 0.0) {
+            System.out.println("Q2");
+        } else if (x < 0.0 && y < 0.0) {
+            System.out.println("Q3");
+        }  else if (x == 0.0 && y == 0.0) {
+            System.out.println("Origem");
+        } else if (x == 0.0) {
+            System.out.println("Eixo X");
+        } else {
+            System.out.println("Eixo Y");
+        }
+
+        sc.close();
+    }
+}
