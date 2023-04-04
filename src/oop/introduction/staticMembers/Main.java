7ea04fce0ae6e736d0335198768984ed54ef7367
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-04 20:01:57 -0300

    Learning Java ./oop/introduction/staticMembers/Main.java - 2023-04-04 20:01:57

---
+++ src/oop/introduction/staticMembers/Main.java
@@ -0,0 +1,26 @@
+package oop.introduction.staticMembers;
+
+public class Main {
+    /*
+     * Static members are members of a class that are not associated with any
+     * particular instance of that class. They are associated with the class
+     * itself. Static members are shared by all instances of the class.
+     *
+     * Static members are declared using the static `keyword`.
+     * Static members are accessed using the class name.
+     * Static members are initialized when the class is loaded.
+     * Static members are used to implement class-level functionality.
+     * Static members are used to implement constants:
+     *  public static final String CONSTANT = "Good practice in java, constants are in uppercase = CONSTANT_CONSTANT";
+     *
+     * Common applications of static members:
+     * - Constants
+     * - Utility classes
+     *
+     * A class that contains only static members can be a static class.
+     * */
+
+    public static void main(String[] args) {
+
+    }
+}
