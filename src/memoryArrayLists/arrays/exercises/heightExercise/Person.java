299032edd64ce56b6c91f3d50e6c799c8cc733f8
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-01 12:28:42 -0300

    Learning Java ./memoryArrayLists/arrays/exercises/heightExercise/Person.java - 2023-05-01 12:28:42

---
+++ src/memoryArrayLists/arrays/exercises/heightExercise/Person.java
@@ -0,0 +1,25 @@
+package memoryArrayLists.arrays.exercises.heightExercise;
+
+public class Person {
+    private final String name;
+    private final int age;
+    private final double height;
+
+    public Person(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
