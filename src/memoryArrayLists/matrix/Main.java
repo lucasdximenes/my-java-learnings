0afa5abfeb3c70a15c730d811990d0fa43207283
Lucas Ximenes <lucasdximenes@gmail.com>
2023-05-15 11:23:57 -0300

    Add matrix learning

---
+++ src/memoryArrayLists/matrix/Main.java
@@ -0,0 +1,50 @@
+package memoryArrayLists.matrix;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * Matrix is a two-dimensional array (array of arrays).
+        * The first index is the row and the second index is the column.
+        *
+        * Syntax:
+        *  int[][] matrix = new int[rows][columns];
+        *
+        * Example:
+        * int[][] matrix = new int[3][2];
+        *
+        * 0 0
+        * 0 0
+        * 0 0
+        *
+        * matrix[0][0] = 1;
+        * matrix[0][1] = 2;
+        * matrix[1][0] = 3;
+        * matrix[1][1] = 4;
+        * matrix[2][0] = 5;
+        * matrix[2][1] = 6;
+        *
+        * 1 2
+        * 3 4
+        * 5 6
+        *
+        * int[][] matrix = {
+        *   {1, 2},
+        *   {3, 4},
+        *   {5, 6}
+        * };
+        *
+        * Iterate over a matrix:
+        * int[][] matrix = {
+        *     {1, 2},
+        *     {3, 4},
+        *     {5, 6}
+        * };
+        *
+        * for (int row = 0; row < matrix.length; row++) {
+        *     for (int column = 0; column < matrix[row].length; column++) {
+        *         System.out.println(matrix[row][column]);
+        *     }
+        * }
+        * */
+    }
+}
